7de90c445e5acf74d93c859c86cd4496a0e15860
unknown <201880186@psu.palawan.edu.ph>
2022-03-29 04:39:32 +0800

    changes made

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '6d4c9863881fae4b0e3051d68003d648fdaf49fa',
+        'name' => 'jesfert/logapppabon',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jesfert/logapppabon' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '6d4c9863881fae4b0e3051d68003d648fdaf49fa',
+            'dev_requirement' => false,
+        ),
+    ),
+);
